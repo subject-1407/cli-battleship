259623f9e91bee3ce19931daedc41208935ac9bb
subject <subject@threepixels.de>
2026-01-05 17:32:25 +0100

    Added Meaningfull setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='cli-battleship',
+	version='0.1',
+	packages=['cli-battleship'],
+	url='https://github.com/subject-1407/cli-battleship',
+	license='MIT License',
+	author='subject',
+	author_email='subject@threepixels.de',
+	description='Command-line implementation of Battleship.'
+)
